af6e5c4e4cef56a266803f4b3882c56b89a97a3d
Tazkia Damayanti <tazkiadamayanti@gmail.com>
2023-10-18 14:36:41 +0700

    done

--- CodeLab/tugas2/script.js
+++ CodeLab/tugas2/script.js
@@ -1,43 +1,32 @@
 const VALID_CLASS = "valid";
 const ERROR_CLASS = "error";
 
-const firstNameInput = document.getElementById("first-name-input");
-const firstNameError = document.getElementById("first-name-error");
-const emptyFirstNameError = document.getElementById("empty-first-name");
-
-const lastNameInput = document.getElementById("last-name-input");
-const lastNameError = document.getElementById("last-name-error");
-const emptyLastNameError = document.getElementById("empty-last-name");
+const fullNameInput = document.getElementById("full-name-input");
+const fullNameError = document.getElementById("full-name-error");
+const emptyFullNameError = document.getElementById("empty-full-name");
 
 const emailInput = document.getElementById("email-input");
 const emailError = document.getElementById("email-error");
 const emptyEmailError = document.getElementById("empty-email");
+
+const addressInput = document.getElementById("address-input");
+const addressError = document.getElementById("address-error");
+const emptyAddressError = document.getElementById("empty-address");
 
 const submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent form submission
 
-  // Validasi nama depan
-  if (firstNameInput.value.trim() === "") {
-    emptyFirstNameError.classList.remove("hide");
-    firstNameError.classList.add(ERROR_CLASS);
-    firstNameInput.classList.add(ERROR_CLASS);
+  // Validasi nama lengkap
+  if (fullNameInput.value.trim() === "") {
+    emptyFullNameError.classList.remove("hide");
+    fullNameError.classList.add(ERROR_CLASS);
+    fullNameInput.classList.add(ERROR_CLASS);
   } else {
-    emptyFirstNameError.classList.add("hide");
-    firstNameError.classList.remove(ERROR_CLASS);
-    firstNameInput.classList.remove(ERROR_CLASS);
-  }
-
-  // Validasi nama belakang
-  if (lastNameInput.value.trim() === "") {
-    emptyLastNameError.classList.remove("hide");
-    lastNameError.classList.add(ERROR_CLASS);
-    lastNameInput.classList.add(ERROR_CLASS);
-  } else {
-    emptyLastNameError.classList.add("hide");
-    lastNameError.classList.remove(ERROR_CLASS);
-    lastNameInput.classList.remove(ERROR_CLASS);
+    emptyFullNameError.classList.add("hide");
+    fullNameError.classList.remove(ERROR_CLASS);
+    fullNameInput.classList.remove(ERROR_CLASS);
   }
 
   // Validasi email
@@ -54,6 +43,18 @@ submitButton.addEventListener("click", function (event) {
     emptyEmailError.classList.add("hide");
     emailError.classList.add("hide");
     emailInput.classList.remove(ERROR_CLASS);
+  }
+
+  // Validasi alamat
+  const addressValue = addressInput.value.trim();
+  if (addressValue === "") {
+    emptyAddressError.classList.remove("hide");
+    addressError.classList.add(ERROR_CLASS);
+    addressInput.classList.add(ERROR_CLASS);
+  } else {
+    emptyAddressError.classList.add("hide");
+    addressError.classList.remove(ERROR_CLASS);
+    addressInput.classList.remove(ERROR_CLASS);
   }
 });
 
